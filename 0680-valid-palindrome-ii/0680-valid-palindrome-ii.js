74b94bb69095f3a239fd008cffa803b7be688458
Sameeha Ansari <sameehaansari011292@gmail.com>
2026-01-16 14:36:10 +0530

    Time: 6 ms (72.32%), Space: 61.6 MB (61.49%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+   let i=0;
+    let j=s.length-1;
+    
+    while(i<j){
+        if(s[i] !== s[j]){
+            return (isPalandrome(s,i+1,j) || isPalandrome(s,i,j-1))
+        }
+        i++;
+        j--
+    }
+    return true
+  
+};
+
+
+function isPalandrome(s,left,right){
+    while(left <right){
+        if(s[left] !== s[right]) return false;
+        left++
+        right--
+    }
+    return true
+}
