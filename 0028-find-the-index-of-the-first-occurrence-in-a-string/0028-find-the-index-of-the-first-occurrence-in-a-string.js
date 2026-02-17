21bc6c3b798f1c0db76fbdffb3e75fda306d887d
Sameeha Ansari <sameehaansari011292@gmail.com>
2026-02-17 13:52:24 +0530

    Time: 0 ms (100%), Space: 53.7 MB (75.04%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,9 +4,6 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    for(let i=0;i<=haystack.length-needle.length;i++){
-        if(haystack.substring(i,i+needle.length)===needle){
-            return i
-        }
-    }return -1
+    let firstDoc = haystack.indexOf(needle)
+    return firstDoc
 };
