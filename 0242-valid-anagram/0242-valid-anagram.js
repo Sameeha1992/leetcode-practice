ba50f6523f89b122e3c023b2b3babaf3076636dd
Sameeha Ansari <sameehaansari011292@gmail.com>
2026-06-10 10:33:58 +0530

    Time: 18 ms (35.16%), Space: 57.8 MB (47.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,19 +4,11 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-let freq={}
-if(s.length !== t.length) return false
-for(let i=0;i<s.length;i++){
-    freq[s[i]] = (freq[s[i]] ||0)+1
-}
+let sWord = s.split("").sort().join("");
+let tWord = t.split("").sort().join("");
 
-for(let j=0;j<t.length;j++){
-    if(freq[t[j]]){
-        freq[t[j]]--
-    }else{
-        return false
-    }
+if(sWord !== tWord){
+    return false
 }
-
 return true
 };
