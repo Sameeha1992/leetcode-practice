0184916d9b322f834b8efbdd1e8f0061d989e647
Sameeha Ansari <sameehaansari011292@gmail.com>
2026-07-14 11:06:02 +0530

    Time: 12 ms (80.67%), Space: 62 MB (55.13%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+
+    let freq={};
+    let res=""
+    for(let i=0;i<s.length;i++){
+        freq[s[i]] = (freq[s[i]] ||0)+1;
+    }
+
+    let sortedData = Object.entries(freq).sort((a,b)=>b[1] -a[1]);
+   for(let [key,count] of sortedData){
+    for(let i=0;i<count;i++){
+       res+=key
+    }
+   }
+  
+  return res
+};
