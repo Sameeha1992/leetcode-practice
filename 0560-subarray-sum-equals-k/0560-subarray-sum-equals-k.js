68851bdc0935b2e585686bc32f45080e247a2833
Sameeha Ansari <sameehaansari011292@gmail.com>
2026-07-15 13:08:52 +0530

    Time: 42 ms (28.52%), Space: 66.5 MB (15.17%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -5,16 +5,20 @@
  */
 var subarraySum = function(nums, k) {
 
-    let count=0;
-    for(let i=0;i<nums.length;i++){
-        let sum=0;
-        for(let j=i;j<nums.length;j++){
-            sum+=nums[j];
-            if(sum === k){
-                count++
-            }
+     let count=0;
+    let sum=0
+    
+    let map={0:1};
+
+    for(let num of nums){
+        sum+=num;
+        if(map[sum -k]){
+            count+=map[sum-k]
         }
+
+        map[sum] =(map[sum] ||0)+1
     }
-    return count
+
+return count
     
 };
