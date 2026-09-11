67cf47249575aa4f82c301a3cc2252400d5277bc
Sameeha Ansari <sameehaansari011292@gmail.com>
2026-09-11 11:02:00 +0530

    Time: 5 ms (61.9%), Space: 55.7 MB (97.62%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} digits
+ * @return {number}
+ */
+var totalNumbers = function(digits) {
+    let res=new Set()
+    for(let i=0;i<digits.length;i++){
+        if(digits[i] ===0) continue
+        for(let j=0;j<digits.length;j++){
+            for(let k=0;k<digits.length;k++){
+                if(i===j || i===k || j===k){
+                   continue
+                }
+                if(digits[k] %2 !==0)continue
+                let number = digits[i]*100+digits[j]*10+digits[k]
+                res.add(number)
+               
+            }
+
+        }
+    }
+    return res.size
+};
